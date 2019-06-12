270a9fb819de51afa977ea15eff0112c92bc58d6
Fr0stbyteR <renshihong@hotmail.com>
2019-06-12 15:10:45 +0200

    optimize for Firefox

--- src/index.ts
+++ src/index.ts
@@ -104,7 +104,7 @@ $(async () => {
      * Use import() for webpack code splitting, needs babel-dynamic-import
      */
     const { Faust } = await import("faust2webaudio");
-    const faust = new Faust();
+    const faust = new Faust({ wasmLocation: "../dist/libfaust-wasm.wasm", dataLocation: "../dist/libfaust-wasm.data" });
     await faust.ready;
     /**
      * To save dsp table to localStorage
