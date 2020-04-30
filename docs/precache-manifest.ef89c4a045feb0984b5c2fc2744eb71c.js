9eb0c0a424e43bda70d3b689460d7887c69fa579
Dominique Fober <fober@grame.fr>
2020-04-30 14:34:02 +0200

    site update

---
+++ docs/precache-manifest.ef89c4a045feb0984b5c2fc2744eb71c.js
@@ -0,0 +1,67 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "assets/0cb5a5c0d251c109458c85c6afeffbaa.svg"
+  },
+  {
+    "url": "assets/13685372945d816a2b474fc082fd9aaa.ttf"
+  },
+  {
+    "url": "assets/1ab236ed440ee51810c56bd16628aef0.ttf"
+  },
+  {
+    "url": "assets/261d666b0147c6c5cda07265f98b8f8c.eot"
+  },
+  {
+    "url": "assets/89ffa3aba80d30ee0a9371b25c968bbb.svg"
+  },
+  {
+    "url": "assets/a0369ea57eb6d3843d6474c035111f29.eot"
+  },
+  {
+    "url": "assets/a06da7f0950f9dd366fc9db9d56d618a.woff2"
+  },
+  {
+    "url": "assets/a609dc0f334a7d4e64205247c4e8b97c.ttf"
+  },
+  {
+    "url": "assets/b15db15f746f29ffa02638cb455b8ec0.woff2"
+  },
+  {
+    "url": "assets/bea989e82b07e9687c26fc58a4805021.woff"
+  },
+  {
+    "url": "assets/c1868c9545d2de1cf8488f1dadd8c9d0.eot"
+  },
+  {
+    "url": "assets/c20b5b7362d8d7bb7eddf94344ace33e.woff2"
+  },
+  {
+    "url": "assets/db78b9359171f24936b16d84f63af378.ttf"
+  },
+  {
+    "url": "assets/ec3cfddedb8bebd2d7a3fdf511f7c1cc.woff"
+  },
+  {
+    "url": "assets/ec763292e583294612f124c0b0def500.svg"
+  },
+  {
+    "url": "assets/f89ea91ecd1ca2db7e09baa2c4b156d1.woff"
+  },
+  {
+    "revision": "297989b7c93044ced25f888fa4e979e1",
+    "url": "editor.worker.js"
+  },
+  {
+    "revision": "c5ff54281be7af4b7828",
+    "url": "index.js"
+  },
+  {
+    "url": "js/59f5246d165b91115b41.js"
+  },
+  {
+    "url": "js/c9bc977960e6a6f7c7bf.js"
+  },
+  {
+    "url": "js/eaadba58d4ed3f96894e.js"
+  }
+]);
